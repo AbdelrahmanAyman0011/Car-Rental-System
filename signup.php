55b846910110407e518ea4dd2da384cadf84838f
AbdelrahmanAyman0011 <abdelrahmanayman0011@gmail.com>
2023-12-31 17:37:15 +0200

    fix connection

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+echo "hi!!";
 if (isset($_POST["submit"])) {
     
         $fname = $_POST["fname"];
@@ -14,12 +15,41 @@ if (isset($_POST["submit"])) {
         require_once 'connection.php';
         require_once 'phpFunctions.php';
 
-        if (emailUsed($con, $mail) !== false) {
-            echo '<script>alert("Email is already in use. Please enter a different email.");</script>';
-        } else {
-            createCustomer($con, $fname, $lname, $mail, $phone, $gender, $country, $city, $street, $pass);
-            echo '<script>alert("User successfully registered!");</script>';
+        
+
+        if(emailUsed($con,$mail) !== false){
+            header("location:Signup.html?error=emailUsed");
+            exit();
         }
+
+        createCustomer($con, $fname, $lname, $mail, $phone, $gender, $country, $city, $street, $pass);
+
+
+        /*try {
+            // Connection to the database
+            $dsn = "mysql:host=localhost;dbname=registration";
+            $user = "root";
+            $password = "";
+
+            $con = new PDO($dsn, $user, $password);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // my SQL query
+            $q = "INSERT INTO user(Name, Email, password) VALUES (?, ?, ?)";
+
+            $stmt = $con->prepare($q);
+
+            if ($stmt->execute([$name, $mail, $pass])) {
+                echo "Hello, $name";
+            } else {
+                echo "Error executing the query.";
+            }
+
+            $con = null;
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }*/
+
 
 }else{
     header("location:Signup.html");
