a286afec0e6e71da5e27b2f51867ece70c3403e9
AhmedSabryOfficail <ahmedsabryibrahime1@gmail.com>
2024-01-03 04:36:54 +0200

    Reservation Price: Number of Days Calculation

--- displayPrice.php
+++ displayPrice.php
@@ -1,7 +1,31 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $selectedValue = $_POST["carType"];
+    if (isset($_POST["startDate"]) && !empty($_POST["startDate"])) {
+        if (isset($_POST["endDate"]) && !empty($_POST["endDate"])) {
+            require_once 'connection.php';
+            require_once 'phpFunctions.php';
+        $sDate = $_POST["startDate"];   
+        $eDate = $_POST["endDate"];   
+        $startDate = new DateTime($sDate);
+        $endDate = new DateTime($eDate);
+        $interval = $startDate->diff($endDate);
+        $numberOfDays = $interval->days;
+        $priceData=displayPrice($con,$selectedValue);
 
+        $totalPrice = $numberOfDays * $priceData;
+        $optionsJson = json_encode($totalPrice);
+
+        echo $optionsJson;
+        exit();
+    }
+    }
+ 
+
+        
+
+        // Calculate total price for the specific period
+    
     require_once 'connection.php';
     require_once 'phpFunctions.php';
 
